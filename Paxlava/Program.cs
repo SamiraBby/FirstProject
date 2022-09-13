cedd5175af2ed97a8bf3c72f7c0424a65cda17cf
SamiraBby <samiraob@code.edu.az>
2022-09-13 22:54:41 +0400

    first commit

---
+++ Paxlava/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Paxlava
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("isdediyiniz sayi daxil edin : ");
+            int say = int.Parse(Console.ReadLine());
+           
+            for (int i = 1; i <= say; i++)
+            {
+                for (int k = say; k >= i; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+
+                }
+                Console.WriteLine();
+
+            }
+
+            
+            for (int i = (say-1); i >= 1; i--)
+            {
+                for (int k = say; k >= i; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
